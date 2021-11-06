9bcf68ea08e20ccf2c1846ae4c6ca9a413b8d7ba
Julen Garro <julengarro@opendeusto.es>
2021-11-06 09:18:06 +0100

    Tercer commit - Modificación en la distribución de ficheros + función de navegar

--- gramolApp/gramolApp/main.cpp
+++ gramolApp/gramolApp/main.cpp
@@ -6,62 +6,19 @@
 //
 
 #include <iostream>
+#include "menuApp.hpp"
 using namespace std;
 
-void registrarCancion()
-{
-    cout << "Has seleccionado la opción 1";
-}
 
-void escucharCancion()
-{
-    
-}
-
-void informacionArtista()
-{
-    
-}
 
 int main(int argc, const char * argv[])
 {
     // insert code here...
+    
     cout << "Pulse un número para acceder al menú correspondiente" << endl;
-    // La opción 1 permitirá registrar una canción.
-    cout << "1 - Registrar canción" << endl;
     
-    // La segunda opción enviará a un link en youtube que ejecutará y se escuchará la canción.
-    cout << "2 - Escuchar canción" << endl;
-    
-    // La tercera opción muestra en pantalla la información relativa al artista.
-    cout << "3 - Información artistas" << endl;
-    
-    cout << "4 - Salir" << endl;
-    // El usuario marca una opción para ir al menú adecuado.
-    int eleccion;
-    cin >> eleccion;
-
-    do
-    {
-        switch (eleccion) {
-            case 1:
-                registrarCancion();
-                break;
-            case 2:
-                escucharCancion();
-                break;
-            case 3:
-                informacionArtista();
-                break;
-            case 4:
-                cout << "Has seleccionado salir de la aplicación" << endl;
-                break;
-            default:
-                cout << "Inserta un número válido" << endl;
-                cin >> eleccion;
-                break;
-        }
-    }while(eleccion != 4);
+    //Es la función declarada en menuApp
+    menuNavigation();
     
     return 0;
 }
---
+++ gramolApp/gramolApp/menuApp.hpp
@@ -0,0 +1,15 @@
+//
+//  menuApp.hpp
+//  gramolApp
+//
+//  Created by Julen Garro Olkotz on 6/11/21.
+//
+
+#ifndef menuApp_hpp
+#define menuApp_hpp
+
+#include <stdio.h>
+
+void menuNavigation();
+
+#endif /* menuApp_hpp */
